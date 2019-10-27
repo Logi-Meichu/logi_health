457bb88d9934036ed99648169d09a2006ffd0450
CS6 <sduum82@gmail.com>
2019-10-27 13:02:45 +0800

    WIP

---
+++ src/test.js
@@ -0,0 +1,21 @@
+
+
+const {BrowserWindow} = require('electron').remote
+const newWindowBtn = document.getElementById('frameless-window')
+
+const path = require('path')
+
+newWindowBtn.addEventListener('click', (event) => {
+  const modalPath = path.join('file://', __dirname, './modal.html')
+  let win = new BrowserWindow({ frame: false })
+
+  win.on('close', () => { win = null })
+  win.loadURL(modalPath)
+  win.show()
+})
+// transparenttrue
+
+// var win = new BrowserWindow({
+//   transparent: true,
+//   frame: false
+// })
